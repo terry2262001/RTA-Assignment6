0525834d06f150205e3ad35acd0e6165e316333a
tery <terynguyen2262001@gmail.com>
2023-03-14 16:32:33 +0700

    update

--- app/src/main/java/com/example/rta_assignment6/Fragment/ListFragment.java
+++ app/src/main/java/com/example/rta_assignment6/Fragment/ListFragment.java
@@ -1,45 +1,31 @@
 package com.example.rta_assignment6.Fragment;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.rta_assignment6.Adapter.RegionInfoAdapter;
 import com.example.rta_assignment6.MainActivity;
 import com.example.rta_assignment6.Model.RegionInfo;
+import com.example.rta_assignment6.Model.ViewRegionModel;
 import com.example.rta_assignment6.R;
-import com.example.rta_assignment6.RegionList;
-import com.google.android.material.tabs.TabLayout;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,40 +45,56 @@ public class ListFragment extends Fragment  implements RegionInfoAdapter.onItemC
      TabLayout tabLayout ;
     SendMessage SM;
 //    private OnListItemClickListener listener;
+private ViewRegionModel viewRegionModel;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        queue = Volley.newRequestQueue(getContext());
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.GET,
-                "https://rtlab02.rtworkspace.com/api/query/datamodel?dm_name=test_ucdp_ged181&token=secret&limit=10&offset=0",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Gson gson = new GsonBuilder().create();
-                        //      System.out.println(response);
-                        Type regionInfoListType = new TypeToken<List<RegionInfo>>(){}.getType();
-                        List<RegionInfo> regionInfoListfromGson = gson.fromJson(response, regionInfoListType);
-                        regionList.addAll(regionInfoListfromGson);
-                        regionInfoAdapter.notifyDataSetChanged();
+//        queue = Volley.newRequestQueue(getContext());
+//        StringRequest stringRequest = new StringRequest(
+//                Request.Method.GET,
+//                "https://rtlab02.rtworkspace.com/api/query/datamodel?dm_name=test_ucdp_ged181&token=secret&limit=10&offset=0",
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        Gson gson = new GsonBuilder().create();
+//                        //      System.out.println(response);
+//                        Type regionInfoListType = new TypeToken<List<RegionInfo>>(){}.getType();
+//                        List<RegionInfo> regionInfoListfromGson = gson.fromJson(response, regionInfoListType);
+//                        regionList.addAll(regionInfoListfromGson);
+//                        regionInfoAdapter.notifyDataSetChanged();
+//
+//
+//
+//
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+//
+//                    }
+//                }
+//
+//        );
+//        queue.add(stringRequest);
+         viewRegionModel = new ViewRegionModel(getContext());
+        // Create the observer which updates the UI.
+        viewRegionModel.getDatafromAPI();
+        viewRegionModel.getMyData().observe(this, new Observer<List<RegionInfo>>() {
+            @Override
+            public void onChanged(List<RegionInfo> regionInfos) {
+                regionList.addAll(regionInfos);
+                regionInfoAdapter.notifyDataSetChanged();
+            }
+        });
 
 
 
 
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
 
-                    }
-                }
-
-        );
-        queue.add(stringRequest);
 
     }
 
---
+++ app/src/main/java/com/example/rta_assignment6/Model/ViewRegionModel.java
@@ -0,0 +1,69 @@
+package com.example.rta_assignment6.Model;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+
+public class ViewRegionModel extends ViewModel {
+    private MutableLiveData<List<RegionInfo>> myData = new MutableLiveData<>();
+    RequestQueue queue;
+    private Context context;
+
+    public ViewRegionModel(Context context) {
+        this.context = context;
+    }
+
+    public Collection<? extends RegionInfo> getDatafromAPI(){
+        queue = Volley.newRequestQueue(context);
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                "https://rtlab02.rtworkspace.com/api/query/datamodel?dm_name=test_ucdp_ged181&token=secret&limit=10&offset=0",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Gson gson = new GsonBuilder().create();
+                        Type regionInfoListType = new TypeToken<List<RegionInfo>>(){}.getType();
+                        List<RegionInfo> regionInfoListfromGson = gson.fromJson(response, regionInfoListType);
+                        myData.postValue(regionInfoListfromGson);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(context, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+        );
+        queue.add(stringRequest);
+
+        return null;
+    }
+
+    public MutableLiveData<List<RegionInfo>> getMyData() {
+        return myData;
+    }
+
+    public void setMyData(MutableLiveData<List<RegionInfo>> myData) {
+        this.myData = myData;
+    }
+}
+
